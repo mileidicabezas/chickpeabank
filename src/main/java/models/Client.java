08b27dd03201bad3da4ab212a9b3343b38a658f6
mileidicabezas <mi.ladydi@hotmail.com>
2021-03-18 10:45:48 +0000

    git add class Client with  the fiels a methods

---
+++ src/main/java/models/Client.java
@@ -0,0 +1,41 @@
+package models;
+import Enum.MaritalStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+    Account account;
+    private String idNumber;
+    private String clientName;
+    private MaritalStatus maritalStatus;
+    private List<Account> accountsList = new ArrayList<Account>();
+
+    public Client(String id, String name, MaritalStatus maritalStatus){
+        this.idNumber = id;
+        this.clientName = name;
+        this.maritalStatus = maritalStatus;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public MaritalStatus getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public List<Account> getAccountsList() {
+        return accountsList;
+    }
+
+    public String print() {
+        String detail = "Id Number: "+this.getIdNumber()+"\nName: "+this.getClientName()+
+                "\nMarital Status: "+this.getMaritalStatus()+ "\nAccount number: " + getAccountsList();
+        return detail;
+    }
+}

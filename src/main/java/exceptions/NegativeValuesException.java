79c221ae6bfb1401c678b860de6d4862baf548f6
mileidicabezas <mi.ladydi@hotmail.com>
2021-03-18 11:24:08 +0000

    add the exception NegativeValuesException for negative value int the depisit amount

---
+++ src/main/java/exceptions/NegativeValuesException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class NegativeValuesException extends Exception{
+    @Override
+    public String getMessage() {
+        return "Can't deposit negative values";
+    }
+}

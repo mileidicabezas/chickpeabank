98420d15395ddc51b634e3c77ddb59d3e356fa52
mileidicabezas <mi.ladydi@hotmail.com>
2021-03-18 11:25:22 +0000

    add method for deposit and withdraw in Account

--- src/main/java/models/CurrentAccount.java
+++ src/main/java/models/CurrentAccount.java
@@ -8,7 +8,27 @@ public class CurrentAccount extends Account {
         this.accountNumber = accountNumber;
     }
 
-        @Override
+    @Override
+    public void deposit(double depositAmount)  {
+        final double MINIMAL_DEPOSIT_VALUE = 0;
+        double percentageAmount = 1.0;
+        double percentageTotal = 100.0;
+        if (depositAmount < MINIMAL_DEPOSIT_VALUE) {
+
+        }
+        if (depositAmount == MINIMAL_DEPOSIT_VALUE) {
+
+        }
+        percentageAmount = (depositAmount *  percentageAmount) / percentageTotal;
+        this.balance = (this.balance + depositAmount) - percentageAmount;
+    }
+
+    @Override
+    public void withdraw(double withdrawalAmount) {
+
+    }
+
+    @Override
         public String print() {
             return null;
         }

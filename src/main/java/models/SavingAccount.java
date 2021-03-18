7b780a36a853a3b905b2bd6480d5aee1b11761d5
mileidicabezas <mi.ladydi@hotmail.com>
2021-03-18 11:10:47 +0000

    add fail and constructor for SavingAccount

---
+++ src/main/java/models/SavingAccount.java
@@ -0,0 +1,15 @@
+package models;
+
+public class SavingAccount extends Account {
+    final double INTEREST_AMOUNT = 0.0001;
+
+    public SavingAccount(String accountNumber){
+        this.interest = INTEREST_AMOUNT;
+        this.accountNumber = accountNumber;
+    }
+
+    @Override
+    public String print() {
+        return null;
+    }
+}

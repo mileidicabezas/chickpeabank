cd637ae3220fc8389b89e9d58c3c0ddcc25392ca
mileidicabezas <mi.ladydi@hotmail.com>
2021-03-22 18:34:58 +0000

    add error messge for value out of range in the withdraw method

--- src/main/java/exceptions/OutRangeValuesException.java
+++ src/main/java/exceptions/OutRangeValuesException.java
@@ -1,4 +1,8 @@
 package exceptions;
 
 public class OutRangeValuesException extends Throwable {
+    @Override
+    public String getMessage() {
+        return "Can't exceed the maximum range  to withdraw";
+    }
 }

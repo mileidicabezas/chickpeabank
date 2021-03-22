a9b279fd8a04505f956a4b68f858ce578ed0735a
mileidicabezas <mi.ladydi@hotmail.com>
2021-03-22 18:27:06 +0000

    add imports for negative values exception

--- src/main/java/models/CurrentAccount.java
+++ src/main/java/models/CurrentAccount.java
@@ -1,20 +1,21 @@
 package models;
 
+import exceptions.NegativeValuesException;
+
 public class CurrentAccount extends Account {
     final double INTEREST_AMOUNT = 0.00015;
-
     public CurrentAccount(String accountNumber){
         this.interest = INTEREST_AMOUNT;
         this.accountNumber = accountNumber;
     }
 
     @Override
-    public void deposit(double depositAmount)  {
+    public void deposit(double depositAmount) throws NegativeValuesException {
         final double MINIMAL_DEPOSIT_VALUE = 0;
         double percentageAmount = 1.0;
         double percentageTotal = 100.0;
         if (depositAmount < MINIMAL_DEPOSIT_VALUE) {
-
+          throw new NegativeValuesException();
         }
         if (depositAmount == MINIMAL_DEPOSIT_VALUE) {
 

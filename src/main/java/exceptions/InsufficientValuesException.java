661c1df3cdd8e1763f608d21d54a9b19893d9a31
mileidicabezas <mi.ladydi@hotmail.com>
2021-03-22 18:34:02 +0000

    add error messge for indufficirnt values in the withdraw method

--- src/main/java/exceptions/InsufficientValuesException.java
+++ src/main/java/exceptions/InsufficientValuesException.java
@@ -1,4 +1,8 @@
 package exceptions;
 
 public class InsufficientValuesException extends Throwable {
+    @Override
+    public String getMessage() {
+        return "values equal to zero are not allowed";
+    }
 }
